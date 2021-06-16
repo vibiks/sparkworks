d85f22d384ad55baacc289935869f98c928d83e7
hduser <hduser@Inceptez>
2021-06-16 10:47:21 +0530

    First Commit

---
+++ src/main/scala/com/inceptez/javaops/Lab01.scala
@@ -0,0 +1,21 @@
+package com.inceptez.javaops
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+
+object Lab01 {
+  
+  def main(args:Array[String])=
+  {
+    val sc = new SparkContext(new SparkConf().setAppName("Lab01").setMaster("local"))
+    
+    val rdd = sc.textFile("file:/home/hduser/test.txt")
+    
+    rdd.foreach(println)
+    
+    println("Hello Scala...")
+    
+  }
+  
+}
